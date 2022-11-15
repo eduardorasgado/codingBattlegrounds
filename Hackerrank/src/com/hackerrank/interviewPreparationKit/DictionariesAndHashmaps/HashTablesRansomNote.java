9c6abfc03cf172613de70345d1c8f5985cac5776
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2022-11-15 01:35:54 -0600

    class for hash tables ransom note challenge was created

---
+++ Hackerrank/src/com/hackerrank/interviewPreparationKit/DictionariesAndHashmaps/HashTablesRansomNote.java
@@ -0,0 +1,15 @@
+package com.hackerrank.interviewPreparationKit.DictionariesAndHashmaps;
+
+import java.util.List;
+
+public class HashTablesRansomNote {
+
+	public static void main(String[] args) {
+		
+		checkMagazine(null, null);
+	}
+
+	public static void checkMagazine(List<String> magazine, List<String> note) {
+		//
+	}
+}
